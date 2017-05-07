3352632b2cc1cea504f1d79e11f748288c7319dd
Jorge Gutiérrez <jorge.gutierrez@hpe.com>
2017-05-07 18:47:32 +0200

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
-import { GoogleMap, GoogleMaps, LatLng, GoogleMapsEvent, CameraPosition, Marker, MarkerOptions} from '@ionic-native/google-maps';
+import { NavController, Platform, LoadingController, ToastController } from 'ionic-angular';
+import { GoogleMap, LatLng, GoogleMapsEvent, MyLocation, CameraPosition, Marker, MarkerOptions} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
@@ -9,58 +9,68 @@ import { GoogleMap, GoogleMaps, LatLng, GoogleMapsEvent, CameraPosition, Marker,
 export class HomePage {
 	public map: GoogleMap;
 	public mapRendered: Boolean = false;
-
-  constructor(public navCtrl: NavController, public platform: Platform) {
+  public location: LatLng;
+  public myLocation: MyLocation;
+  constructor(public navCtrl: NavController, public platform: Platform, public loadingCtrl: LoadingController, public toastCtrl: ToastController) {
   	this.platform.ready().then(() => {
-  		this.showMap();
+  		this.getMyLocation();
   	})
   }
 
   showMap(){
-  	let location = new LatLng(47.6062,-122.3321);
-  	this.map = new GoogleMap('map',{
-  		'camera': {
-  			'latLng': location,
-  			'tilt': 30,
-  			'zoom': 15,
-  			'bearing': 50
-  		}
-  	});
+  
+  	  		this.mapRendered=true;
 
-  	this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-  		this.mapRendered=true;
-  	});
+
+        let position: CameraPosition = {
+          target: this.myLocation.latLng,
+          zoom: 15
+        };
+
+        this.map.moveCamera(position);
+        let markerOptions: MarkerOptions = {
+          'position': this.myLocation.latLng
+        };
+
+
+    let toast = this.toastCtrl.create({
+      message: 'Location found, search for anything',
+      position: 'bottom',
+      duration: 3000
+    });
+
+    toast.present();
+
+
   }
 
   getMyLocation(){
+    this.map = new GoogleMap('map');
+
     console.log("Getting location");
-  	this.map.getMyLocation().then((location) => {
-      console.log(location);
-  		var msg = [
-  			"I am here:\n",
-  			"latitute:" + location.latLng.lat,
-  			"longitude:" + location.latLng.lng].join("\n");
-
-  			let position: CameraPosition = {
-  				target: location.latLng,
-  				zoom: 15
-  			};
-  			this.map.moveCamera(position);
-  			let markerOptions: MarkerOptions = {
-  				'position': location.latLng,
-  				'title': msg
-  			};
-  			this.map.addMarker(markerOptions).then((
-  				marker: Marker) => {
-  					marker.showInfoWindow();
-  			}
-        );
-  	}, (err) => {
-          console.log(err);
-          alert(err.error_message);
+    let loader = this.loadingCtrl.create({
+      content: "Getting location ... please wait."
     });
-  }
 
-
-
+    loader.present();
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+        	this.map.getMyLocation().then((location) => {
+            console.log(location);
+            this.myLocation = location;
+            this.showMap();
+            loader.dismiss();
+        	}, (err) => {
+                loader.dismiss();
+                console.log(err);
+                alert(err.error_message);
+          });
+    }, (err) => {
+            loader.dismiss();
+            console.log("Error in MAP_READY event");
+            console.log(err);
+            alert(err.error_message);
+      });
+    }
+  
+  
 }
